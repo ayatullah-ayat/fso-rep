03347824fd5f3b8273cd52a28ad8f3eef39ce4eb
Ayatullah Ayat <ayat2486@gmail.com>
2023-03-18 20:28:52 +0600

    env added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -13,6 +14,7 @@ const requestLogger = (req, res, next) => {
 }
 app.use(express.json());
 
+app.use(cors());
 app.use(requestLogger);
 
 let notes = [
