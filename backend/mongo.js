bc41b3c8c1bfa7b00ca68fedbaa2fa8115272c41
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-03-22 01:32:39 +0600

    mongo setup

---
+++ backend/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+
+console.log(process.argv);
+const url = `mongodb+srv://ayat2486:jPF5kYRBrtykTJnA@clusterfso1.1zjsm5m.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema);
+
+Note.find({}).then(res => {
+    res.forEach(note => {
+        console.log(note);
+    })
+    mongoose.connection.close();
+})
+
+// const note = new Note({
+//     content: 'HTML is fantastic',
+//     important: true,
+// })
+
+// note.save().then(res => {
+//     console.log('Note saved');
+//     mongoose.connection.close();
+// })
