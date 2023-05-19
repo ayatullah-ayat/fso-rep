2a14a1b993d72ac54410c1bec2327c1ffd801416
ayat2486@gmail.com <ayat2486@gmail.com>
2023-05-19 21:39:58 +0600

    login form included

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,52 @@ import './App.css';
 function App() {
 
   const [notes, setNotes] = useState([]);
- 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   useEffect(() => {
     let request = getAll();
-    
+
     request.then(res => setNotes(res.data));
   }, []);
-  
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('user', username, password);
+  }
+
   return (
     <div className="App">
+
+      <div className='container'>
+        
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="email">User Name</label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={username} 
+              onChange={({ target }) => setUsername(target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input 
+              type="password" 
+              className="form-control"
+              value={password} 
+              onChange={({ target }) => setPassword(target.value)}/>
+        </div>
+        <div className='mt-3'>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+
       <h1>Notes</h1>
-      <ul>
-        { notes.map(note => <li key={note.id}>{note.content}</li>) }
+      <ul className='list-group'>
+        {notes.map(note => <li className='list-group-item' key={note.id}>{note.content}</li>)}
       </ul>
+      </div>
     </div>
   );
 }
--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = '/api/notes';
+const baseUrl =  process.env.REACT_APP_BASE_URL + '/api/notes';
 const getAll = async () => {
     const request = await axios.get(baseUrl);
 
