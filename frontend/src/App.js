31e439d32ca33b9e8e32cf3447faf24f148fcfc3
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-05-22 01:40:26 +0600

    token base authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react';
-import getAll from './services/notes';
+import noteService from './services/notes';
 import loginService from "./services/login";
 
 import './App.css';
@@ -8,12 +8,12 @@ function App() {
 
     const [notes, setNotes] = useState([]);
     const [user, setUser] = useState(null);
+    const [newNote, setNewNote] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     useEffect(() => {
-        let request = getAll();
-
+        let request = noteService.getAll();
         request.then(res => setNotes(res.data));
     }, []);
 
@@ -23,6 +23,9 @@ function App() {
         try {
             const user = await loginService.login({ username, password });
 
+            window.localStorage.setItem('loggedUser', JSON.stringify(user));
+
+            noteService.setToken(user.token);
             setUser(user);
             setUsername('');
             setPassword('');
@@ -30,6 +33,17 @@ function App() {
         catch (exception) {
             console.log('handleLogin_exception', exception);
         }
+    }
+
+    const handleNote = async (event) => {
+        event.preventDefault();
+
+        console.log('content=========', newNote);
+        if(newNote) {
+            const response = await noteService.create({content: newNote})
+            console.log('created_note', response);
+        }
+
     }
 
 
@@ -61,10 +75,17 @@ function App() {
 
     const NoteForm = () => {
         return (
-            <form>
+            <form onSubmit={handleNote}>
                 <div className="form-group">
                     <label htmlFor="note">Note</label>
-                    <input type="text" className="form-control" />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        value={newNote}
+                        onChange={({ target }) => setNewNote(target.value)} />
+                </div>
+                <div className='mt-3'>
+                    <button type="submit" className="btn btn-primary">Save Note</button>
                 </div>
             </form>
         );
--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,11 +1,27 @@
 import axios from "axios"
 
 const baseUrl =  process.env.REACT_APP_BASE_URL + '/api/notes';
+let token = null;
+
 const getAll = async () => {
     const request = await axios.get(baseUrl);
 
     return request;
 }
 
+const setToken = (strToken) => {
+    token = strToken;
+}
 
-export default getAll;
+const create = async (newObject) => {
+    const config = {
+        headers: {
+            Authorization: token
+        }
+    }
+
+    const response = await axios.post(baseUrl, newObject, config);
+    return response.data;
+}
+
+export default { getAll, create, setToken };
