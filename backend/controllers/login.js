658f661862ec95a52054f2bec50d687e6650bca3
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-05-21 23:27:01 +0600

    Note Form included

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -5,16 +5,15 @@ const User = require('../models/user');
 
 
 loginRouter.post('/', async (request, response) => {
-    const {username, password} = request.body;
+    const { username, password } = request.body;
 
     const user = await User.findOne({ username });
 
-    const passwordCorrect = user === null 
+    const passwordCorrect = user === null
         ? false
         : await bcrypt.compare(password, user.passwordHash);
-    
-    if(!(user && passwordCorrect))
-    {
+
+    if (!(user && passwordCorrect)) {
         return response.status(401).json({
             error: 'invalid username or password'
         })
@@ -25,15 +24,15 @@ loginRouter.post('/', async (request, response) => {
         id: user._id,
     }
 
-    const token = jwt.sign(userFortoken, process.env.SECRET, {expiresIn: 60*60});
+    const token = jwt.sign(userFortoken, process.env.SECRET, { expiresIn: 60 * 60 });
 
     response
-            .status(200)
-            .send({
-                token, 
-                username: user.username,
-                name: user.name
-            })
+        .status(200)
+        .send({
+            token,
+            username: user.username,
+            name: user.name
+        })
 });
 
 module.exports = loginRouter;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,89 @@
 
 import { useState, useEffect } from 'react';
 import getAll from './services/notes';
+import loginService from "./services/login";
+
 import './App.css';
 function App() {
 
-  const [notes, setNotes] = useState([]);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [notes, setNotes] = useState([]);
+    const [user, setUser] = useState(null);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    let request = getAll();
+    useEffect(() => {
+        let request = getAll();
 
-    request.then(res => setNotes(res.data));
-  }, []);
+        request.then(res => setNotes(res.data));
+    }, []);
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    console.log('user', username, password);
-  }
+    const handleLogin = async (event) => {
+        event.preventDefault();
 
-  return (
-    <div className="App">
+        try {
+            const user = await loginService.login({ username, password });
 
-      <div className='container'>
-        
-      <form onSubmit={handleLogin}>
-        <div className="form-group">
-          <label htmlFor="email">User Name</label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={username} 
-              onChange={({ target }) => setUsername(target.value)}/>
+            setUser(user);
+            setUsername('');
+            setPassword('');
+        }
+        catch (exception) {
+            console.log('handleLogin_exception', exception);
+        }
+    }
+
+
+    const LoginForm = () => {
+        return (
+            <form onSubmit={handleLogin}>
+                <div className="form-group">
+                    <label htmlFor="email">User Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={username}
+                        onChange={({ target }) => setUsername(target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)} />
+                </div>
+                <div className='mt-3'>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        )
+    }
+
+    const NoteForm = () => {
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="note">Note</label>
+                    <input type="text" className="form-control" />
+                </div>
+            </form>
+        );
+    }
+
+    return (
+        <div className="App">
+
+            <div className='container'>
+                { user === null && <LoginForm />}
+
+                <h1>Notes</h1>
+                <ul className='list-group'>
+                    {notes.map(note => <li className='list-group-item' key={note.id}>{note.content}</li>)}
+                </ul>
+                <NoteForm />
+            </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input 
-              type="password" 
-              className="form-control"
-              value={password} 
-              onChange={({ target }) => setPassword(target.value)}/>
-        </div>
-        <div className='mt-3'>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </div>
-      </form>
-
-      <h1>Notes</h1>
-      <ul className='list-group'>
-        {notes.map(note => <li className='list-group-item' key={note.id}>{note.content}</li>)}
-      </ul>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
