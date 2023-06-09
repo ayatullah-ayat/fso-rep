ef3dfb067ddcbc952df0b2e4b453dc66bbe1e613
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-06-09 10:10:30 +0600

    Remodified User component

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,4 +1,12 @@
 
-const User = () => <div>User Page</div>
+const User = () => <div>
+    <h2>Users List</h2>
+    <ul>
+        <li>user 1</li>
+        <li>user 2</li>
+        <li>user 3</li>
+        <li>user 4</li>
+    </ul>
+</div>
 
 export default User;
